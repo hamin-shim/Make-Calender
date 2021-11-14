0998593e42a04a23b706bee74b471b08ad1d40c6
alsgur7859 <alsgur7859@naver.com>
2021-11-15 01:01:05 +0900

    일정 등록해서 파이어베이스에 저장

--- src/component/Month.js
+++ src/component/Month.js
@@ -1,26 +1,94 @@
 import MonthlyCalender from "./MonthlyCalender"
 import "../style/month.scss"
 import { useParams } from "react-router"
+import React, { useState } from "react";
+import { firestore } from "./fbase";
+
 const Month = ()=>{
     const {id} = useParams();
+    
+    const [date, setDate] = useState("");
+    const [start_time, setStart_time] = useState("");
+    const [end_time, setEnd_time] = useState("");
+    const [title, setTitle] = useState("");
+    const [tag, setTag] = useState("");
+    const [description, setDescription] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+        firestore.collection("events").add({
+            date,
+            start_time,
+            end_time,
+            title,
+            tag,
+            description,
+            createdAt: Date.now()
+
+        });
+        setDate("");
+        setStart_time("");
+        setEnd_time("");
+        setTitle("");
+        setTag("");
+        setDescription("");
+    };
+    const onChangeDate = (event) => {
+        const{
+            target: { value },
+        } = event;
+        setDate(value);
+    };
+    const onChangeStart_time = (event) => {
+        const{
+            target: { value },
+        } = event;
+        setStart_time(value);
+    };    
+    const onChangeEnd_time = (event) => {
+        const{
+            target: { value },
+        } = event;
+        setEnd_time(value);
+    }; 
+    const onChangeTitle = (event) => {
+        const{
+            target: { value },
+        } = event;
+        setTitle(value);
+    };
+    const onChangeTag = (event) => {
+        const{
+            target: { value },
+        } = event;
+        setTag(value);
+    };
+    const onChangeDescription = (event) => {
+        const{
+            target: { value },
+        } = event;
+        setDescription(value);
+    };
+
+
     return(
         <div className="container monthScreen">
             <div className="row">
         <MonthlyCalender date={id}/>
         <div className="col-lg-3 col-md-6 col-8 make">
-            <form>
+            <form onSubmit = {onSubmit}>
                 <label for = "date">일자</label>
-                <input name = "date" type="date" placeholder="2000-00-00"/>
+                <input value={date} onChange={onChangeDate} name = "date" type="date" placeholder="2000-00-00"/>
                 <label for = "start_time">시작 시간</label>
-                <input name = "start_time" type="time" placeholder="00:00"/>
+                <input value={start_time} onChange={onChangeStart_time} name = "start_time" type="time" placeholder="00:00"/>
                 <label for = "end_time">종료 시간</label>
-                <input name = "end_time" type="time" placeholder="00:00"/>
+                <input value={end_time} onChange={onChangeEnd_time} name = "end_time" type="time" placeholder="00:00"/>
                 <label for = "title">제목</label>
-                <input name = "title" type="text" placeholder="제목을 입력하세요"/>
+                <input value={title} onChange={onChangeTitle} name = "title" type="text" placeholder="제목을 입력하세요"/>
                 <label for = "tag">태그</label>
-                <input name = "tag" type="text" placeholder="#insert #study"/>
+                <input value={tag} onChange={onChangeTag} name = "tag" type="text" placeholder="#insert #study"/>
                 <label for = "description">설명</label>
-                <textarea name = "description" className="description" placeholder="어떤 일정인가요?" cols="50" rows="5"/>
+                <textarea value={description} onChange={onChangeDescription} name = "description" className="description" placeholder="어떤 일정인가요?" cols="50" rows="5"/>
+                <input type="submit" value="Register" />
             </form>
         </div>
         </div>
