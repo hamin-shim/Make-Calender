604f670a8260639803ba6dd00721911825107370
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-11-17 23:39:35 +0900

    search startt!

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 function Search(){
+    const [searchWhat, setSearchWhat] = useState(null);
     return(
+        <div>
         <h1>검색 기능</h1>
+        <form>
+            <input className="form-control" type="text" value={searchWhat} onChange={(e)=>{
+                setSearchWhat(e.target.value);
+            }} placeholder="What do you want to find for?"/>
+            <input type="submit" value="Search"/>
+        </form>
+        </div>
     )
 }
 export default Search;
