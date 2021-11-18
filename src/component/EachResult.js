9ec109c8208cf3bff12c119fdc5a640a137733d7
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-11-18 13:19:00 +0900

    simple design

---
+++ src/component/EachResult.js
@@ -0,0 +1,15 @@
+import { useHistory } from "react-router"
+
+export default ({data})=>{
+    const history = useHistory();
+
+    return(
+        <div onClick={()=>{
+            history.push(`/month/${data.date}`)//클릭시 그 달 달력으로 이동. 
+        }} className="eachResult">
+            <h3>{data.title}</h3>
+            <h4>{data.date}</h4>
+            <h5>{data.description}</h5>
+        </div>
+    )
+}
--- src/component/Result.js
+++ src/component/Result.js
@@ -1,16 +1,14 @@
 import eventlist from "../data/eventlist";
-
+import EachResult from "./EachResult";
+import "../style/result.scss";
 export default ({data})=>{
     return(
-        <div>{data.map((each)=>{
+        <div className="result">
+            {data.length===0 ? <h3>검색 결과가 없습니다</h3> : null }
+            {data.map((each)=>{
             if(each){
                 return(
-                    <div>
-                        <h1>{each.title}</h1>
-                        <h2>{each.date}</h2>
-                        <h3>{each.description}</h3>
-                        <hr/>
-                    </div>
+                        <EachResult data={each} />
                 )
             }
         })}</div>
