3beb3f784c567320ffcf4502e5db4433f2e193b5
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-11-18 11:39:14 +0900

    데이터 넘기기

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import eventlist from "../data/eventlist";
+import Event from "../data/eventlist";
 import "../style/search.scss";
+import Result from "./Result";
 function Search(){
     const [searchWhat, setSearchWhat] = useState(null);
+    const [result, setResult] = useState(null);
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        setResult(searchWhat)
+    }
+    const searchResult = ()=>{
+        Event.map((event)=>{
+        console.log(event)
+    })}
+    useEffect(()=>{
+        searchResult()
+    })
     return(
         <div className="search">
         <h1>검색 기능</h1>
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="searchBar">
             <input className="form-control" type="text" value={searchWhat} onChange={(e)=>{
                 setSearchWhat(e.target.value);
@@ -13,6 +28,10 @@ function Search(){
             <input type="submit" value="Search"/>
             </div>
         </form>
+        {
+
+        }
+        <Result data = {result}/>
         </div>
     )
 }
