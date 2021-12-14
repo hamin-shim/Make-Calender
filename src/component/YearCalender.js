9c295d3239c51488d6dd65028aa390ec941ac0c0
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-12-15 00:50:03 +0900

    자잘한 수정(for 포폴)

--- src/component/YearCalender.js
+++ src/component/YearCalender.js
@@ -1,74 +1,111 @@
 import moment from "moment";
 import { useState } from "react";
 import { useHistory } from "react-router";
-import "../style/year.scss"
-export default ()=>{
-    const [year, setYear] = useState(moment());
-    const thisYear = year;
-    const history = useHistory();
-    let firstMonth = thisYear.clone().startOf('year');
-    // let lastMonth = thisYear.clone().endOf('year');
-    let monthArray = [];
-    for(let i=0; i<12;i++){
-        monthArray.push(firstMonth.clone().add(i,'month'))
-    }
-    const onClick = (e)=>{
-        const {nativeEvent:{path:{length}}} = e;
-        const datePath = e.nativeEvent.path[length-11].className.substring(6,13);
-        history.push(`/month/${datePath}`)
-    }
-    const calendarArr = (month) => {
-        let result = [];
-        let firstWeek = month.clone().startOf('month').week();
-        let lastWeek = month.clone().endOf('month').week() === 1 ? 53 : month.clone().endOf('month').week();
-        let week = firstWeek;
-        for (week ; week <= lastWeek ; week++){
-            result = result.concat(
-                <div className={`${week} week`} key={week}>
-                    {
-                        Array(7).fill(0).map((data, index)=>{
-                            let wrongNum = false;
-                            let days = firstMonth.clone().startOf('year').week(week).startOf('week').add(index,'day');
-                            if((week === firstWeek && parseInt(days.format('D'))>8)||(week===lastWeek && parseInt(days.format('D'))<8)){
-                                wrongNum = true;
-                            }
-                            return(
-                                <div className={`day ${wrongNum} ${week} ${index}`} key={index}>{(wrongNum) ? null : days.format('D')}</div>
-                            )
-                        })
-                    }
+import "../style/year.scss";
+export default () => {
+  const [year, setYear] = useState(moment());
+  const thisYear = year;
+  const history = useHistory();
+  let firstMonth = thisYear.clone().startOf("year");
+  // let lastMonth = thisYear.clone().endOf('year');
+  let monthArray = [];
+  for (let i = 0; i < 12; i++) {
+    monthArray.push(firstMonth.clone().add(i, "month"));
+  }
+  const onClick = (e) => {
+    const {
+      nativeEvent: {
+        path: { length },
+      },
+    } = e;
+    const datePath = e.nativeEvent.path[length - 12].className.substring(6, 13);
+    history.push(`/month/${datePath}`);
+  };
+  const calendarArr = (month) => {
+    let result = [];
+    let firstWeek = month.clone().startOf("month").week();
+    let lastWeek =
+      month.clone().endOf("month").week() === 1
+        ? 53
+        : month.clone().endOf("month").week();
+    let week = firstWeek;
+    for (week; week <= lastWeek; week++) {
+      result = result.concat(
+        <div className={`${week} week`} key={week}>
+          {Array(7)
+            .fill(0)
+            .map((data, index) => {
+              let wrongNum = false;
+              let days = firstMonth
+                .clone()
+                .startOf("year")
+                .week(week)
+                .startOf("week")
+                .add(index, "day");
+              if (
+                (week === firstWeek && parseInt(days.format("D")) > 8) ||
+                (week === lastWeek && parseInt(days.format("D")) < 8)
+              ) {
+                wrongNum = true;
+              }
+              return (
+                <div className={`day ${wrongNum} ${week} ${index}`} key={index}>
+                  {wrongNum ? null : days.format("D")}
                 </div>
-            )
-        }
-        return(result)
-    }
-    return(
-        <div className="home">
-            <div className="control">
-                <button className="btn btn-outline-secondary" onClick={()=>{setYear(year.clone().subtract(1,'year'))}}>◁◁</button>
-                <span>{year.format('YYYY년')}</span>
-                <button className="btn btn-outline-secondary" onClick={()=>{setYear(year.clone().add(1,'year'))}}>▷▷</button>
-            </div>
-            <div className="monthLab container-fluid">
-                <div className="row">
-            {monthArray.map((month,i)=>(
-                <div onClick={onClick} className={`month ${year.format('YYYY')}-${String(i+1).padStart(2,0)} col-lg-3 col-md-4 col-5`}>
-                    <h1>{i+1}월</h1>
-                    <div className="week dayName">
-                            <div className="day sun">S</div>
-                            <div className="day">M</div>
-                            <div className="day">T</div>
-                            <div className="day">W</div>
-                            <div className="day">T</div>
-                            <div className="day">F</div>
-                            <div className="day sat">S</div>
-                    </div>
-                    {calendarArr(month)}
-                </div>
-            ))}
-                </div>
-            </div>
-            
+              );
+            })}
         </div>
-    )
-}
+      );
+    }
+    return result;
+  };
+  return (
+    <div className="home">
+      <div className="control">
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => {
+            setYear(year.clone().subtract(1, "year"));
+          }}
+        >
+          ◁◁
+        </button>
+        <span>{year.format("YYYY년")}</span>
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => {
+            setYear(year.clone().add(1, "year"));
+          }}
+        >
+          ▷▷
+        </button>
+      </div>
+      <div className="monthLab container">
+        <div className="row">
+          {monthArray.map((month, i) => (
+            <div className="oneMonthLab col-xl-3 col-lg-4 col-6">
+              <div
+                onClick={onClick}
+                className={`month ${year.format("YYYY")}-${String(
+                  i + 1
+                ).padStart(2, 0)}`}
+              >
+                <h1>{i + 1}월</h1>
+                <div className="week dayName">
+                  <div className="day sun">S</div>
+                  <div className="day">M</div>
+                  <div className="day">T</div>
+                  <div className="day">W</div>
+                  <div className="day">T</div>
+                  <div className="day">F</div>
+                  <div className="day sat">S</div>
+                </div>
+                {calendarArr(month)}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,54 +1,65 @@
 import { useEffect, useState } from "react";
 import "../style/search.scss";
 import Result from "../component/Result";
-function Search({userObject, events}){
-    const [searchWhat, setSearchWhat] = useState(null);
-    const [keyWord, setKeyWord] = useState("")
-    const [result, setResult] = useState([]);
-    const onSubmit = (e)=>{
-        e.preventDefault();
-        setKeyWord(searchWhat)
-    }
-    const searchResult = (keyWord)=>{
-        let includedData = []
-        if(!userObject){
-            alert("로그인 해주세요")
-        }else{
-            events.map((eve)=>{
-            if(eve.creatorId===userObject.uid){
-                if((eve.title.includes(keyWord))|| //title에 포함되거나
-                eve.description.includes(keyWord)|| //description에 포함되거나
-                (eve.date.includes(keyWord)|| //date에 포함되어 있거나
-                (eve.tag.includes(keyWord)))){ //tag array에 포함되어 있거나
-                    includedData.push(eve)
-                }
-            }
-            })
+function Search({ userObject, events }) {
+  const [searchWhat, setSearchWhat] = useState(null);
+  const [keyWord, setKeyWord] = useState("");
+  const [result, setResult] = useState([]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setKeyWord(searchWhat);
+  };
+  const searchResult = (keyWord) => {
+    let includedData = [];
+    if (!userObject) {
+      alert("로그인 해주세요");
+    } else {
+      events.map((eve) => {
+        if (eve.creatorId === userObject.uid) {
+          if (
+            eve.title.includes(keyWord) || //title에 포함되거나
+            eve.description.includes(keyWord) || //description에 포함되거나
+            eve.date.includes(keyWord) || //date에 포함되어 있거나
+            eve.tag.includes(keyWord)
+          ) {
+            //tag array에 포함되어 있거나
+            includedData.push(eve);
+          }
         }
-        setResult(includedData)
+      });
     }
-    useEffect(()=>{
-        searchResult( )
-    },[])
-    useEffect(()=>{
-        searchResult(keyWord)
-        console.log(searchWhat, keyWord)
-    },[keyWord])
-    return(
-        <div className="search mt-5" >
-        <form onSubmit={onSubmit}>
-            <div className="searchBar">
-            <input className="form-control" type="text" value={searchWhat} onChange={(e)=>{
-                setSearchWhat(e.target.value);
-            }} placeholder="검색어를 입력하세요 (태그, 제목, 설명 등..)"/>
-            <input type="submit" value="Search"/>
-            </div>
-        </form>
-        {
-
-        }
-        <Result data = {result}/>
+    setResult(includedData);
+  };
+  useEffect(() => {
+    searchResult();
+  }, []);
+  useEffect(() => {
+    searchResult(keyWord);
+    console.log(searchWhat, keyWord);
+  }, [keyWord]);
+  return (
+    <div className="search mt-5">
+      <form onSubmit={onSubmit}>
+        <div className="searchBar">
+          <input
+            className="form-control"
+            type="text"
+            value={searchWhat}
+            onChange={(e) => {
+              setSearchWhat(e.target.value);
+            }}
+            placeholder="검색어를 입력하세요 (태그, 제목, 설명 등..)"
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-outline-primary"
+          />
         </div>
-    )
+      </form>
+      {}
+      <Result data={result} />
+    </div>
+  );
 }
 export default Search;
